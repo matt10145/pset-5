af0755d76c083703f6a5a293a93fe346629bc30a
matth101 <mhui@ucvts.org>
2019-12-07 23:10:11 -0500

    finish all exercises except EC

--- js/shapes.js
+++ js/shapes.js
@@ -60,7 +60,10 @@ const drawRectangle = function() {
         var x = Number(prompt("X: "));
         var y = Number(prompt("Y: "));
         
-        if (width < 1 || width > canvas.width) {
+        if (width == 0 && height == 0 && x == 0 && y == 0) {
+            terminate = true;
+        }
+        else if (width < 1 || width > canvas.width) {
             alert("Your width must be between 1 and 1024.");
             terminate = false;
         }
@@ -80,15 +83,16 @@ const drawRectangle = function() {
             alert("Your rectangle won't fit on the canvas.");
             terminate = false;
         }
-        else if (width == null || height == null || x == null || y == null) {
+        else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+            alert("One of your values is not a number.");
+            terminate = false;
+        }
+        else {
             terminate = true;
         }
-
     } while (terminate == false);
 
     context.strokeRect(x, y, width, height);
-
-    // this loop does not end
 }
 
 /*
@@ -96,7 +100,30 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    // write your exercise 3 code here
+    const canvas = document.getElementById('student-canvas-3');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    let terminate;
+    do {
+        var color = prompt("Color: ");
+        color = color.toLowerCase();
+
+        if (color === 0) {
+            terminate = true;
+        }
+        else if (color !== "black" && color !== "blue" && color !== "green" && color !== "orange" && color !== "purple" && color !== "red" && color !== "yellow") {
+            alert(`${color} is not a supported color.`);  
+            context.clearRect(0, 0, canvas.width, canvas.height);         
+            terminate = false;  
+        }
+        else {
+            terminate = true;
+        }
+    } while (terminate == false);
+
+    context.fillStyle = color;
+    context.fillRect(10, 10, 100, 50);
 };
 
 /*
@@ -104,15 +131,115 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    // write your exercise 4 code here
-};
+    const canvas = document.getElementById('student-canvas-4');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    let terminate;
+    do {
+        var side1 = Number(prompt("Side 1: "));
+        var side2 = Number(prompt("Side 2: "));
+        var side3 = Number(prompt("Side 3: "));
+
+        if (side1 === 0 || side2 === 0 || side3 === 0){
+            terminate = true;
+        }
+        else if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+            alert("One of your sides is not a number.");
+            terminate = false;
+        }
+        else if (Math.hypot(side1, side2) != side3) {
+            alert("That's not a valid right triangle.");
+            terminate = false;
+        }
+        else if (side1 > canvas.height || side1 > canvas.width || side1 < 1) {
+            alert("Your triangle won't fit on the canvas.");
+            terminate = false;
+        }
+        else if (side2 > canvas.height || side2 > canvas.width || side2 < 1) {
+            alert("Your triangle won't fit on the canvas.");
+            terminate = false;
+        }
+        else if (side3 > canvas.height || side3 > canvas.width || side3 < 1) {
+            alert("Your triangle won't fit on the canvas.");
+            terminate = false;
+        }
+        else {
+            terminate = true;
+        }
+    } while (terminate == false);
+
+    let height = Math.min(side1, side2, side3);
+    let hypotenuse = Math.max(side1, side2, side3);
+    let base = Math.sqrt(((hypotenuse ** 2) - (height ** 2)));
+
+    if (side1 !== 0 && side2 !== 0 && side3 !== 0) {
+        context.beginPath();
+        context.moveTo(25, 25);
+        context.lineTo(25, (25 + height));
+        context.lineTo((25 + base), (25 + height));
+        context.closePath();
+        context.stroke();    
+    }
+};  
 
 /*
  * Exercise 5.
  */
 
 const drawFace = function() {
-    // write your exercise 4 code here
+    const canvas = document.getElementById('student-canvas-5');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const PI = Math.PI;
+    let centerEye;
+    let terminate;
+    do {
+        var radius = Number(prompt("Radius: "));
+
+        if (radius === 0) {
+            terminate = true;
+        }
+        else if (isNaN(radius)) {
+            alert("Your radius is not a number.");
+            terminate = false;
+        }
+        else if (radius > 256) {
+            alert("Your smiley face won't fit on the canvas.");
+            terminate = false;
+        }
+        else if (radius < 32) {
+            alert("Your radius must be at least 32.");
+            terminate = false;
+        }
+        else {
+            terminate = true;
+        }
+    } while (terminate == false);
+
+    radiusEye = radius * 0.15;
+    radiusMouth = radius * 0.70;
+    centerEye = radius * 0.40;
+    
+    if (radius !== 0) {
+        context.beginPath();
+        context.arc(512, 256, radius, 0, (2*PI));
+        context.stroke();
+        context.closePath();
+        context.beginPath();
+        context.arc((512 - centerEye), (256 - centerEye), radiusEye, 0, (2*PI));
+        context.stroke();
+        context.closePath();
+        context.beginPath();
+        context.arc((512 + centerEye), (256 - centerEye), radiusEye, 0, (2*PI));
+        context.stroke();
+        context.closePath();
+        context.beginPath();
+        context.arc(512, 256, radiusMouth, 0, PI);
+        context.stroke();
+        context.closePath();
+    }
 };
 
 /*
@@ -120,5 +247,48 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    // write your exercise 5 code here
-};
+    const canvas = document.getElementById('student-canvas-6');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const SIZE_UPPER_THRESHOLD = 100.4;
+    let terminate;
+    do {
+        var side = Number(prompt("Side: "));
+
+        if (side === 0) {
+            terminate = true;
+        }
+        else if (isNaN(side)) {
+            alert("Your block size is not a number.");
+            terminate = false;
+        }
+        else if (side < 1) {
+            alert("Your block size must be at least 1.");
+            terminate = false;
+        }
+        else if (size > SIZE_UPPER_THRESHOLD) {
+            alert("Your pyramid won't fit on the canvas.");
+            terminate = false;
+        }
+        else {
+            terminate = true;
+        }
+    } while (terminate == false);
+
+
+
+    
+
+
+    /*
+     * bottom left origin is 10, 502
+     * height upper threshold is 502/5 = 100.4
+     * 
+     * bash out squares:
+     * draw base line and then draw upwards at intervals of the side length
+     * connect the two points
+     * rinse, repeat
+     * (for loop for the upward lines)
+     */
+}
